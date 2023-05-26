63e47f671f644c56668af90da944c6906d34679f
himanshush26 <himanshu.sharma@deeporion.com>
2023-05-26 17:26:16 +0530

    active records

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "Himanshu", password: "12345", except: [:index, :show]
+
+  http_basic_authenticate_with name: "himanshu", password: "12345", except: [:index, :show]
 
   def index
     @articles = Article.all
